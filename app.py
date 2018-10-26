6c456ff588b2c406d0a2c41a3e22843ff908cbf0
Aparna Karve <aparna.karve@gmail.com>
2018-10-25 17:22:50 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-url = 'http://127.0.0.1:5000'
+url = 'http://my-aiops-publisher-my-aiops2.5a9f.insights-dev.openshiftapps.com'
 data = '{  "platform": {    "login": {      "userName": "name",      "password": "pwd"    }  } }'
 response = requests.post(url, data=data,headers={"Content-Type": "application/json"})
 print(response)
